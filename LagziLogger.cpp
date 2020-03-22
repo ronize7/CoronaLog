45d4f2e997492d0571ebba550c42a75f4f55ca50
lglomer <omerdov63@gmail.com>
2020-03-22 14:38:35 +0200

    Added LagziLogger

---
+++ LagziLogger.cpp
@@ -0,0 +1,9 @@
+//
+//  LagziLogger.cpp
+//  Playground
+//
+//  Created by Omer Lagziel on 22/03/2020.
+//  Copyright © 2020 Lagzi. All rights reserved.
+//
+
+#include "LagziLogger.hpp"
---
+++ LagziLogger.hpp
@@ -0,0 +1,14 @@
+//
+//  LagziLogger.hpp
+//  Playground
+//
+//  Created by Omer Lagziel on 22/03/2020.
+//  Copyright © 2020 Lagzi. All rights reserved.
+//
+
+#ifndef LagziLogger_hpp
+#define LagziLogger_hpp
+
+#include <stdio.h>
+
+#endif /* LagziLogger_hpp */
