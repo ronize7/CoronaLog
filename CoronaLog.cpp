442bc01931dbdd443e9f31629488a2252c941bd5
naorhaziz <naorhaziz@gmail.com>
2020-03-22 12:01:54 +0200

    Added NaorLogger usage.

--- CoronaLog.cpp
+++ CoronaLog.cpp
@@ -1,10 +1,13 @@
 #include "Common/Common.hpp"
+#include "NaorLogger/NaorLogger.hpp"
 
 int main()
 {
     std::cout << ("start logging!") << std::endl;
 
-
+	NaorLogger naor_logger(L"C:\\temp\\log.txt");
+	naor_logger.log(L"Hello ");
+	naor_logger.log(L"World!");
 
 
     std::cout << ("stop logging!") << std::endl;
